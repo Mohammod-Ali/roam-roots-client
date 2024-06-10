9f5c605ad402ba06409d6c48eb78bab248482880
Mohammod Ali <mohammodali651@gmail.com>
2024-06-10 23:45:46 +0600

    text added on carousel

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -6,6 +6,12 @@ const Banner = () => {
           src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg"
           className="w-full"
         />
+        <div className="absolute flex items-center h-full bottom-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+         <div className="text-white space-y-7 pl-12 w-1/2">
+            <h3 className="text-5xl font-bold">Flash Sale!</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quis est iste, repudiandae eveniet aperiam dignissimos temporibus recusandae nulla, natus numquam accusantium, officiis quidem mollitia voluptate deleniti. Explicabo, error excepturi.</p>
+         </div>
+        </div>
         <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
           <a href="#slide4" className="btn btn-circle mr-4">
             ❮
